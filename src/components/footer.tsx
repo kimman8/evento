a7e8ca7ce54077c5dca9e5c29d89f3677227d7ae
KIM YUEN <kim_man@live.com>
2024-09-06 13:05:42 +1000

    feat: improve layout, home page, and footer with enhanced styling and dynamic links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link';
+
+const routes = [
+  { name: 'Terms & Conditions', path: '/terms-conditions' },
+  { name: 'Privacy Policy', path: '/privacy-policy' },
+];
 export default function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="mt-auto flex justify-between items-center border-t border-white/10 h-16 px-3 sm:px-9 text-sm text-white/25">
+      <small className="text-xs">
+        &copy; Kim Man 2024. All rights reserved.
+      </small>
+      <ul className="flex gap-x-3 sm:gap-x-8">
+        {routes.map((route) => (
+          <li key={route.path} className="text-blue-500 hover:underline">
+            <Link href={route.path}>{route.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
