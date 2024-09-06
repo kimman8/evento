173279f753c0755e1e0be4793e3c8dd492a3ad4f
KIM YUEN <kim_man@live.com>
2024-09-06 16:59:33 +1000

    feat: decode city from URL and capitalise first letter of each word

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,27 @@
 import H1 from '@/components/h1';
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+export default function EventsPage({ params }: EventsPageProps) {
+  const decodedCity = decodeURIComponent(params.city);
+  // Capitalise first letter of each word
+  const capitaliseWords = (str) => {
+    return str
+      .split(' ') // Split the string by spaces
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalise first letter of each word
+      .join(' '); // Join the words back together
+  };
+
+  const formattedCity = capitaliseWords(decodedCity);
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <H1>Events in melbourne</H1>
+      <H1>
+        {params.city === 'all' ? 'All events' : `Events in ${formattedCity}`}
+      </H1>
     </main>
   );
 }
