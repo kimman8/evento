3513569ac0547d6d69c188d37ce9de75d23f588e
KIM YUEN <kim_man@live.com>
2024-09-07 18:50:40 +1000

    feat: integrate tailwind-merge and refactor H1 component for dynamic class handling

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -26,7 +26,7 @@ export default async function EventsPage({ params }: EventsPageProps) {
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <H1>
+      <H1 className="mb-28">
         {params.city === 'all' ? 'All events' : `Events in ${formattedCity}`}
       </H1>
       <EventsList events={events} />
