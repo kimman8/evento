ac718947df1622b5903df5164872e4ac7e3c4f87
KIM YUEN <kim_man@live.com>
2024-09-07 19:29:56 +1000

    feat: fetch event data based on event slug

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,14 @@
-export default function EventPage() {
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+export default async function EventPage({ params }: EventPageProps) {
+  const slug = params.slug;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+  console.log(event);
   return <main>Event Page</main>;
 }
