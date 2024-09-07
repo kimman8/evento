8242769d27d4d0e668edc49d1226a0b7a8ebb3cc
KIM YUEN <kim_man@live.com>
2024-09-07 15:04:53 +1000

    refactor: improve EventCard component layout and add image rendering

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,4 +1,5 @@
 import { EventoEvent } from '@/lib/types';
+import Image from 'next/image';
 
 type EventCardProps = {
   event: EventoEvent;
@@ -7,10 +8,20 @@ export default function EventCard({ event }: EventCardProps) {
   return (
     <section
       key={event.id}
-      className="bg-white/[10%] rounded-lg p-6 mt-6 w-full sm:w-[580px]"
+      className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden"
     >
-      <h2 className="text-xl font-bold">{event.name}</h2>
-      <p className="text-sm text-white/50 mt-2">{event.location}</p>
+      <Image
+        src={event.imageUrl}
+        width={500}
+        height={280}
+        alt={event.name}
+        className="h-[60%] object-fit"
+      />
+      <div className="flex flex-col items-center justify-center flex-1">
+        <h2 className="text-2xl font-semibold">{event.name}</h2>
+        <p className="italic text-white/75">By {event.organizerName}</p>
+        <p className="text-sm text-white/50 mt-4">{event.location}</p>
+      </div>
     </section>
   );
 }
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -6,7 +6,7 @@ type EventsListProps = {
 };
 export default function EventsList({ events }: EventsListProps) {
   return (
-    <section>
+    <section className="flex gap-10 flex-wrap justify-center max-w-[1100px] px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
