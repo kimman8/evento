dc42041f91bba91cedd514eb88d28b2ba1c2bbfd
KIM YUEN <kim_man@live.com>
2024-09-12 15:52:10 +1000

    refactor: prevent accidental usage of server utility functions on the client

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,5 @@
 import clsx, { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import prisma from './db';
-import { notFound } from 'next/navigation';
-import { unstable_cache } from 'next/cache';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,43 +8,3 @@ export function cn(...inputs: ClassValue[]) {
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
-export const getEvents = unstable_cache(
-  async (decodedCity: string, page = 1) => {
-    const events = await prisma.eventoEvent.findMany({
-      where: {
-        city: decodedCity === 'all' ? undefined : capitalize(decodedCity),
-      },
-      orderBy: {
-        date: 'asc',
-      },
-      take: 6,
-      skip: (page - 1) * 6,
-    });
-
-    let totalCount;
-    if (decodedCity === 'all') {
-      totalCount = await prisma.eventoEvent.count();
-    } else {
-      totalCount = await prisma.eventoEvent.count({
-        where: {
-          city: capitalize(decodedCity),
-        },
-      });
-    }
-
-    return { events, totalCount };
-  }
-);
-
-export const getEvent = unstable_cache(async (slug: string) => {
-  const event = await prisma.eventoEvent.findUnique({
-    where: {
-      slug,
-    },
-  });
-  if (!event) {
-    return notFound();
-  }
-  return event;
-});
